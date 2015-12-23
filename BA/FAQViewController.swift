078324d5b6a8fbd6cee3421056cb0f2da91c0fec
Felizia <fbe@adorsys.de>
2015-12-23 10:17:22 +0100

    created imprint vc with scrollview and faqs vc (not complete)

---
+++ BA/FAQViewController.swift
@@ -0,0 +1,14 @@
+//
+//  FAQViewController.swift
+//  BA
+//
+//  Created by Felizia Bernutz on 22.12.15.
+//  Copyright © 2015 Felizia Bernutz. All rights reserved.
+//
+
+import UIKit
+
+class FAQViewController: UIViewController {
+
+    
+}
---
+++ BA/ImprintViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ImprintViewController.swift
+//  BA
+//
+//  Created by Felizia Bernutz on 22.12.15.
+//  Copyright © 2015 Felizia Bernutz. All rights reserved.
+//
+
+import UIKit
+
+class ImprintViewController: UIViewController {
+
+    
+}
